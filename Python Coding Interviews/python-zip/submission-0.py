37cb7d9ad14f55c28bb00cd40d172d9cfa725d63
Saanvi Kakde <skakde2@asu.edu>
2026-05-14 14:01:49 -0400

    Add: python-zip - submission-0

---
+++ Python Coding Interviews/python-zip/submission-0.py
@@ -0,0 +1,17 @@
+from typing import List, Dict
+
+
+def group_names_and_scores(names: List[str], scores: List[int]) -> Dict[str, int]:
+    
+    grouped = {} # key = name, value = score 
+
+    for name, score in zip(names, scores):
+        grouped[name] = score 
+
+    return grouped 
+
+
+# do not modify below this line
+print(group_names_and_scores(["Alice", "Bob", "Charlie"], [90, 80, 70]))
+print(group_names_and_scores(["Jane", "Carol", "Charlie"], [25, 100, 60]))
+print(group_names_and_scores(["Doug", "Bob", "Tommy"], [80, 90, 100]))
