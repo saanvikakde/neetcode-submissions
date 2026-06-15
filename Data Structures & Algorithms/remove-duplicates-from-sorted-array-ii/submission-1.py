3723c630e3279f1f8e8be11e8d6e158573cf328e
Saanvi Kakde <skakde2@asu.edu>
2026-06-15 17:38:14 -0400

    Add: remove-duplicates-from-sorted-array-ii - submission-1

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array-ii/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+
+        # remove duplicates but have them placed at start of array 
+
+        l = 2
+
+        for r in range(2,len(nums)): 
+            if nums[r] != nums[l-2]:
+                nums[l] = nums[r]
+                l+=1
+        return l 
+
+
+
+
+        
