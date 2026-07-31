b12c7393823098a1159a6da587797e50cbf19885
Saanvi Kakde <skakde2@asu.edu>
2026-07-30 21:51:21 -0400

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        if len(s) <= 1 : return True 
+
+        s = re.sub(r'[^a-zA-Z0-9]', '', s)
+
+        l, r = 0, len(s)-1  
+
+        while r > l: 
+
+            print(f'r {r} l {l}')
+            if s[l].lower() != s[r].lower(): 
+                return False 
+            
+            l += 1 
+            r -= 1
+            
+        return True 
+            
+
+
+
+        
